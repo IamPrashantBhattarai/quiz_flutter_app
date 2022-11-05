f2edddb94b7b75cb9ac5bad5d0b942c8a1714dd6
Prashant <prashantbhattarainepal@gmail.com>
2022-11-05 16:39:06 +0545

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+
+//Creating the object of the quizbrain class .(named as= quizbrain)
+QuizBrain quizBrain = QuizBrain();
+
+void main() {
+  runApp(Quizzler());
+}
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({Key? key}) : super(key: key);
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  //Creating the list scorekeeper and also giving the list items type as widget.
+  List<Icon> scoreKeeper = [];
+
+  void checkAnswer(bool userPickedAnswer) {
+    bool correctAnswer = quizBrain.getQuestionAnswer();
+
+    setState(() {
+      if (correctAnswer == userPickedAnswer) {
+        scoreKeeper.add(
+          Icon(
+            Icons.check,
+            color: Colors.green,
+          ),
+        );
+      } else {
+        scoreKeeper.add(
+          Icon(
+            Icons.close,
+            color: Colors.red,
+          ),
+        );
+      }
+      // quizBrain.nextQuestion();
+    });
+  }
+
+  //Keeping the questions and the answers in the below type list becomes more convenient and easy.
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionText(),
+                //The class quizBrain.questionBank is used to tap in the list of it.
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              // textColor: Colors.white,
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.green,
+              ),
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                checkAnswer(true);
+                setState(() {
+                  quizBrain.nextQuestion();
+                });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              // color: Colors.red,
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.redAccent,
+              ),
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                checkAnswer(false);
+                setState(() {
+                  quizBrain.nextQuestion();
+                });
+                //The user picked false.
+              },
+            ),
+          ),
+        ),
+        //TODO:Add a row at the bottom as your score keeper.
+        Row(
+          children: scoreKeeper,
+        ),
+      ],
+    );
+  }
+}
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
